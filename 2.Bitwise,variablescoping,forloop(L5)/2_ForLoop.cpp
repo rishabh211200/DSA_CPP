3c1539b411984307b41c7f15c0168f48f631f13d
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-03 22:51:14 +0530

    Increment and decrement

---
+++ 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 7;
+    cout<<(++i)<<endl; //Pre increment
+    cout<<(i++)<<endl; // Post increment
+    cout<<(i--)<<endl; // post decrement
+    cout<<(--i)<<endl; // pre decrement
+    return 0;
+}
