027153103fcdab7bb6d5e3de13c1f1e628c661dd
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-04 00:29:44 +0530

    Prime or not and Factors

--- 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
+++ 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
@@ -1,64 +1,51 @@
-// #include<iostream>
-// using namespace std;
+/*
+#include<iostream>
+using namespace std;
 
-// int main()
-// {
-//     int i = 7;
-//     cout<<(++i)<<endl; //Pre increment
-//     cout<<(i++)<<endl; // Post increment
-//     cout<<(i--)<<endl; // post decrement
-//     cout<<(--i)<<endl; // pre decrement
-//     return 0;
-    
-// }
-
-// #include<iostream>
-// using namespace std;
-
-// int main()
-// {
-//     //Find output 
-//     int a, b=1;
-//     a=10;
-//     if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
-//     cout<<b;
-//     else
-//     cout<< ++b;
-//     return 0;
-// }
-
-
+int main()
+{
+    //Find output 
+    int a, b=1;
+    a=10;
+    if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
+    cout<<b;
+    else
+    cout<< ++b;
+    return 0;
+}
+*/
+/*
   ///////////////////////Print 1 to 5
 
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main()
-// {
-//     //Approach 1
-//     // int n = 5;
-//     // for(int i = 1; i<=n;i++)
-//     // {
-//     //     cout<<i<<endl;
-//     // }
-//     /////////////////or
-//     //Approach 2
-//     int n = 5;
-//     int i = 1;
-//     for(;;){
-//         if(i<=n){
-//         cout<<i<<endl;
-//         }
-//         else
-//         break;
-//         i++;
-//     }
-//     return 0;
-// }
-
-
-/////Fibonacci series 10 iteration
+int main()
+{
+    //Approach 1
+    // int n = 5;
+    // for(int i = 1; i<=n;i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+    /////////////////or
+    //Approach 2
+    int n = 5;
+    int i = 1;
+    for(;;){
+        if(i<=n){
+        cout<<i<<endl;
+        }
+        else
+        break;
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+///Fibonacci series 10 iteration
 #include<iostream>
 using namespace std;
 
@@ -76,4 +63,52 @@ int main()
     }
     
     return 0;
+}
+*/
+
+/*********************Prime or not*********************/
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    bool isPrime = 1;
+
+    for(int i = 2; i<n ; i++){
+        //rem = 0, Not a Prime
+        if(n%i==0){
+             isPrime = 0;
+             break;
+        }
+    }
+     if(isPrime == 0){
+        cout<<n<<" "<<"is Not a Prime Number";
+     }
+     else{
+         cout<<n<<" "<< "is a Prime Number";
+     }
+}
+*/
+
+/**********check number of factors of n**********/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int count=0;
+    
+    for(int i = 1; i<=n ; i++){
+        if(n%i==0){
+             count++;
+        }
+    }
+    cout<<count;
 }
