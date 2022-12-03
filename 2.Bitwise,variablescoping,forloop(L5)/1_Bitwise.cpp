ba316ec28e5f82605e1d08216072d101f753260e
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-03 16:19:52 +0530

    Left shift and right shift operator

--- 2.Bitwise,variablescoping,forloop(L5)/1_Bitwise.cpp
+++ 2.Bitwise,variablescoping,forloop(L5)/1_Bitwise.cpp
@@ -3,11 +3,19 @@ using namespace std;
 
 int main()
 {
-    int a = 4;
-    int b = 6;
-    cout<<"a&b"<<"  "<<(a&b)<<endl;
-    cout<<"a|b"<<"  "<<(a|b)<<endl;
-    cout<<"~a"<<"   "<<(~a)<<endl;
-    cout<<"a^b"<<"  "<<(a^b)<<endl;
-    
+
+    // // Bitwise Operator
+    // int a = 4;
+    // int b = 6;
+    // cout<<"a&b"<<"  "<<(a&b)<<endl;
+    // cout<<"a|b"<<"  "<<(a|b)<<endl;
+    // cout<<"~a"<<"   "<<(~a)<<endl;
+    // cout<<"a^b"<<"  "<<(a^b)<<endl;
+
+    // Left shift and right shift operator
+
+    cout<<(17>>1)<<endl;
+    cout<<(17>>2)<<endl;
+    cout<<(19<<1)<<endl;
+    cout<<(21<<2)<<endl;
 }
