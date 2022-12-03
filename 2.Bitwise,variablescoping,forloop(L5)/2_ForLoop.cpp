2dbadb9c9fa96b8f1723d92c6ed4cc3b6972aebf
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-03 23:15:59 +0530

    Find output good question

--- 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
+++ 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
@@ -1,12 +1,28 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int i = 7;
+//     cout<<(++i)<<endl; //Pre increment
+//     cout<<(i++)<<endl; // Post increment
+//     cout<<(i--)<<endl; // post decrement
+//     cout<<(--i)<<endl; // pre decrement
+//     return 0;
+    
+// }
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    int i = 7;
-    cout<<(++i)<<endl; //Pre increment
-    cout<<(i++)<<endl; // Post increment
-    cout<<(i--)<<endl; // post decrement
-    cout<<(--i)<<endl; // pre decrement
+    //Find output 
+    int a, b=1;
+    a=10;
+    if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
+    cout<<b;
+    else
+    cout<< ++b;
     return 0;
 }
