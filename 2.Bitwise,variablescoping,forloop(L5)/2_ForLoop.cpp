07f7025f438ce3eb3b31f5f33c34039b8a9d4c01
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-03 23:38:55 +0530

    ForLoop Approach

--- 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
+++ 2.Bitwise,variablescoping,forloop(L5)/2_ForLoop.cpp
@@ -12,17 +12,47 @@
     
 // }
 
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     //Find output 
+//     int a, b=1;
+//     a=10;
+//     if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
+//     cout<<b;
+//     else
+//     cout<< ++b;
+//     return 0;
+// }
+
+
+  ///////////////////////Print 1 to 5
+
+  
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    //Find output 
-    int a, b=1;
-    a=10;
-    if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
-    cout<<b;
-    else
-    cout<< ++b;
+    //Approach 1
+    // int n = 5;
+    // for(int i = 1; i<=n;i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+    /////////////////or
+    //Approach 2
+    int n = 5;
+    int i = 1;
+    for(;;){
+        if(i<=n){
+        cout<<i<<endl;
+        }
+        else
+        break;
+        i++;
+    }
     return 0;
 }
